fce1e1a02c6c14c66abb3486e973191cdfc7743b
Ana Paula Scariot <anapaula.scariot@hotmail.com>
2019-12-12 12:20:26 -0600

    Políticas en el registro

---
+++ resources/views/politica.blade.php
@@ -0,0 +1,22 @@
+@extends('layout.app')
+
+@section('title', 'Política')
+
+@section('content')
+	<div class="politica container">
+		<br/>
+		<h3>Política de privacidad y uso</h3>
+		<div>
+			@csrf
+			<ul type=”A”>
+			  <li>Cuando se encuentre haciendo uso del sitio web y le sean solicitados datos personales, Usted compartirá esta información sólo con los estudiantes de la Maestría en Sistemas Interactivos Centrados en el Usuario (MSICU), ya que no se compartirá la información confidencial con terceras personas, salvo que sea requerida por orden judicial o que Usted de manera expresa lo autorice de conformidad con la Ley vigente.</li>
+			  <li>El uso del sistema es restrito a los estudiantes de la Maestría en Sistemas Interactivos Centrados en el Usuario y a los usuarios de la empresa Reinke.</li>
+			  <li>Este es un sistema construido para fines educativos.</li>
+			  <li>MSICU puede difundir las estadísticas de acceso a el sitio web, con propósitos lícitos en los casos que marque la ley.</li>
+			  <li>MSICU puede difundir las estadísticas de acceso a el sitio web, con propósitos lícitos en los casos que marque la ley.</li>
+			  <li>Los estudiantes de MSICU se reservan el derecho de realizar actualizaciones a esta política de privacidad por lo que le recomendamos revisar este documento periódicamente para estar enterado de estos cambios.</li>
+			  <li>Esta política es efectiva a partir del 16 de diciembre de 2019.</li>
+			</ul>
+		</div>
+	</div>
+@endsection 
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 Route::group(['middleware' => ['web']], function(){
 
     //Init route's
@@ -17,18 +7,24 @@ Route::group(['middleware' => ['web']], function(){
     Route::post('auth', 'Auth\LoginController@authenticate');
     Route::get('registro', 'Auth\RegisterController@index');
     Route::post('registro', 'Auth\RegisterController@create');
+    Route::get('politica', function() {
+        return view('politica');
+    });
     
     //Here protetected route's
     Route::group(['middleware' => ['auth']], function () {
+
         Route::get('datos', function(){
             return view('input_data_dashboard');
         })->name('datos');
-        Route::get('dashboard', function (){
-            return view('dashboard');
-        });
+
+        Route::get('dashboard', 'DashboardController@index');
+
         Route::get('historial', function(){
             return view('history');
         });
+
         Route::get('salir','Auth\LoginController@logout');
+
     });
 });
