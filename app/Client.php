02164729baa787b8d2d95aab5bd4789e9668f8b6
Ana Paula Scariot <anapaula.scariot@hotmail.com>
2019-12-12 20:52:02 -0600

    Relaciones de los modelos y historial ok

--- app/Client.php
+++ app/Client.php
@@ -8,7 +8,17 @@ class Client extends Model
 {
     //utilizara la tabla clients
 
-    
+    //$users = DB::table('users')
+    //        ->join('contacts', 'users.id', '=', 'contacts.user_id')
+    //        ->join('orders', 'users.id', '=', 'orders.user_id')
+    //        ->select('users.*', 'contacts.phone', 'orders.price')
+    //
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+     
 }
 
 
